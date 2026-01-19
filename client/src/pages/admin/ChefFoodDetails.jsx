72ce0849a26a33e2294167e92ad3579db8cb7900
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2026-01-19 22:45:44 +0530

    updated

--- client/src/pages/admin/ChefFoodDetails.jsx
+++ client/src/pages/admin/ChefFoodDetails.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { BsStarFill } from "react-icons/bs";
+import { CiLocationOn } from "react-icons/ci";
 import { FaChevronLeft, FaRegStar } from "react-icons/fa6";
 import { MdOutlineAccessTime } from "react-icons/md";
 import { TbTruckDelivery } from "react-icons/tb";
@@ -15,7 +17,7 @@ const ChefFoodDetails = () => {
     try {
       const { data } = await axios.get(
         `http://localhost:4000/api/menu/seller/${itemId}`,
-        { withCredentials: true }
+        { withCredentials: true },
       );
       if (data.success) {
         setSellerMenu(data.menu);
@@ -51,20 +53,29 @@ const ChefFoodDetails = () => {
         />
         <div className="py-4" id="_id">
           <div className="flex justify-between item-cneter py-1">
-            <p className="pt-2">{sellerMenu?.name}</p>
-            <p className=""> ${sellerMenu?.price}</p>
+            <p className="pt-2 text-sm font-medium">{sellerMenu?.name}</p>
+            <p className="font-medium"> ${sellerMenu?.price}</p>
           </div>
 
-          <div className="flex justify-between items-center gap-2">
-            <p>{sellerMenu?.restaurant?.address}</p>
-            <div className="flex item-center gap-2 ">
-              <FaRegStar className="text-orange-500 mt-1" size={16}  />
-              <p>{sellerMenu?.restaurant?.rating} (10 Reviews)</p>
+          <div className="flex justify-between text-center items-center gap-2">
+            <p className="text-sm text-gray-500 flex items-center gap-2">
+              <CiLocationOn />
+              {sellerMenu?.restaurant?.address}
+            </p>
+            <div className="flex text-center item-center gap-2 ">
+              <BsStarFill className="text-orange-500 my-1" size={14} />
+              <p>
+                {" "}
+                <span className="font-medium text-orange-500">
+                  {sellerMenu?.restaurant?.rating}
+                </span>{" "}
+                <span className="text-gray-500">(10 Reviews)</span>{" "}
+              </p>
             </div>
           </div>
 
           <div className="flex items-center py-1 gap-2">
-            <p>{sellerMenu?.description}</p>
+            {/* <p>{sellerMenu?.description}</p> */}
           </div>
         </div>
       </div>
