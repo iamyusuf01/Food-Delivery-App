0f9a22fdd48f1317d4f50ad3ad2098db59e0a07a
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2026-02-10 21:52:39 +0530

    fix: update API URL in .env for production environment

--- server/server.js
+++ server/server.js
@@ -26,6 +26,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
 // app.cors(cors());
 app.use(
   cors({
