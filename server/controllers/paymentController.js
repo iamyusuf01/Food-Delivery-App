4a90f595d2fa49ea689211687ec6e833b5c0a445
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2026-04-15 21:03:19 +0530

    feat: add refund functionality and scheduled task for clearing expired payments

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -2,6 +2,7 @@ import { razorpayInstance } from "../config/razorpay.js";
 import Order from "../models/orderModel.js";
 import crypto from "crypto";
 import Payment from "../models/paymentModel.js";
+import cron from 'node-cron'
 
 export const createPaymentOrder = async (req, res) => {
   try {
@@ -74,7 +75,6 @@ export const createPaymentOrder = async (req, res) => {
       razorpayOrder,
       paymentId: payment._id,
     });
-
   } catch (error) {
     console.error("Payment Order Error:", error);
 
@@ -87,11 +87,8 @@ export const createPaymentOrder = async (req, res) => {
 
 export const verifyPayment = async (req, res) => {
   try {
-    const {
-      razorpay_order_id,
-      razorpay_payment_id,
-      razorpay_signature,
-    } = req.body;
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+      req.body;
 
     if (!razorpay_order_id || !razorpay_payment_id || !razorpay_signature) {
       return res.status(400).json({
@@ -153,7 +150,6 @@ export const verifyPayment = async (req, res) => {
       message: "Payment verified successfully",
       payment,
     });
-
   } catch (error) {
     console.error("Verify Payment Error:", error);
 
@@ -164,4 +160,81 @@ export const verifyPayment = async (req, res) => {
   }
 };
 
+export const initiateRefund = async (req, res) => {
+  try {
+    const { paymentId } = req.body;
 
+    if (!paymentId) {
+      return res.status(400).json({
+        success: false,
+        message: "PaymentId is required",
+      });
+    }
+
+    const payment = await Payment.findById(paymentId);
+
+    if (!payment) {
+      return res.status(404).json({
+        success: false,
+        message: "Payment not found",
+      });
+    }
+
+    if (payment.status !== "SUCCESS") {
+      return res.status(400).json({
+        success: false,
+        message: "Only successful payments can be refunded",
+      });
+    }
+
+    if (payment.refundStatus === "PROCESSED") {
+      return res.status(400).json({
+        success: false,
+        message: "Refund already processed",
+      });
+    }
+
+    const refund = await razorpayInstance.payments.refund(
+      payment.razorpayPaymentId,
+      {
+        amount: Math.round(payment.amount * 100), // paise
+      }
+    );
+
+    payment.refundId = refund.id;
+    payment.refundStatus = "PROCESSED";
+    await payment.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Refund successful",
+      refund,
+    });
+
+  } catch (error) {
+    console.error("Refund Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+cron.schedule("*/10 * * * * ", async () => {
+  const expiredPayment = await Payment.find({
+    status: 'PENDING',
+    createdAt: {
+      $lt: new Date(Date.now() - 15 * 60 * 1000),
+
+    }
+  })
+
+  for(const payment of expiredPayments){
+    payment.status = "FAILED",
+    payment.failureReason = 'Timeout';
+    await payment.save();
+  }
+
+  console.log('Expired payments cleared')
+})
--- server/routes/paymentRouter.js
+++ server/routes/paymentRouter.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { createPaymentOrder, verifyPayment } from '../controllers/paymentController.js';
+import { createPaymentOrder, initiateRefund, verifyPayment } from '../controllers/paymentController.js';
 import auth from '../middlewares/authMiddleware.js'
 
 const router = express.Router();
 
 router.post('/create',auth, createPaymentOrder)
 router.post('/verify',auth, verifyPayment)
+router.post('/refund',auth, initiateRefund)
 
 
 export default router
